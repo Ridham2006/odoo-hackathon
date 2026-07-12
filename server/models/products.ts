53d4e16878b8834ce822d82b279837e548c15fba
omaku2006 <omaku2006@gmail.com>
2026-07-12 12:43:15 +0530

    Backend Initialization

---
+++ server/models/products.ts
@@ -0,0 +1,13 @@
+import mongoose from "mongoose";
+
+const productSchema = new mongoose.Schema({
+  name : {
+    type : String,
+    required : true,
+    trim : true,
+  },
+  barcode : {
+    type : Number,
+
+  }
+})
